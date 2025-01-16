23a636d5efe3a59ea57c029b2023ab94e403d57e
Misho506 <maurisho01@gmail.com>
2025-01-15 21:19:45 -0600

    Add social media links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,23 +7,23 @@ const Footer = () => (
   <footer>
     <section>
       <article className="flex flex-col justify-content-center items-center my-6">
-        <section className="flex gap-4">
-          <article className="w-14 h-14 rounded-full bg-yellow-icon flex items-center justify-content-center">
+        <div className="flex gap-4">
+          <div onClick={() => window.open('https://www.instagram.com/paticas_paque_las_quierocr/')} className="w-14 h-14 rounded-full bg-yellow-icon flex items-center justify-content-center">
             <FaInstagram className="text-3xl text-black" />
-          </article>
-          <article className="w-14 h-14 rounded-full bg-yellow-icon flex items-center justify-content-center">
+          </div>
+          <div onClick={() => window.open('https://www.facebook.com/Paticaspaquelasquiero')} className="w-14 h-14 rounded-full bg-yellow-icon flex items-center justify-content-center">
             <FaFacebookF className="text-4xl text-black" />
-          </article>
-          <article className="w-14 h-14 rounded-full bg-yellow-icon flex items-center justify-content-center">
+          </div>
+          <div onClick={() => window.open('https://www.tiktok.com/@paticaspaquelasquiero')} className="w-14 h-14 rounded-full bg-yellow-icon flex items-center justify-content-center">
             <FaTiktok className="text-3xl text-black" />
-          </article>
-        </section>
+          </div>
+        </div>
         <p className="text-gray-400 Italic mt-3">¡Siganos en nuestras redes sociales!</p>
       </article>
     </section >
-    <section className="flex h-20 items-center justify-center rights-text text-white italic">
+    <article className="flex h-20 items-center justify-center rights-text text-white italic">
       <FaRegCopyright className="text- italic text-2xl pr-2 pb-1" />{new Date().getFullYear()}, Paticas paque las quier. Todos los derechos reservados.
-    </section>
+    </article>
   </footer >
 )
 
