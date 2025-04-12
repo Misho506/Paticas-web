3c7b50339c22abe5d7c363ed504d43e3ef5deb3a
Misho506 <maurisho01@gmail.com>
2025-04-11 23:21:34 -0600

    Add limitof dates for the whales tours, 7 days before September and 7 days before October

--- src/components/tourBookingCard/TourBookingCard.tsx
+++ src/components/tourBookingCard/TourBookingCard.tsx
@@ -10,6 +10,10 @@ import { useTour } from "../../context/TourContext";
 import { useNavigate } from "react-router";
 import Modal from "../modal/Modal";
 
+// For the Whales Tour
+const minDate = new Date(new Date().getFullYear(), 7, 25); // August 25
+const maxDate = new Date(new Date().getFullYear(), 8, 23); // September 23
+
 const TourBookingCard = () => {
   const { i18n } = useTranslation();
   const navigate = useNavigate();
@@ -118,7 +122,11 @@ const TourBookingCard = () => {
           </h3>
           <article className="flex-1 relative border rounded-full basis-2/3">
             <LuCalendarClock className="z-10 absolute h-5 w-5 top-2.5 left-3" />
-            <DatePicker className="w-full rounded-full pl-9 pr-4 py-2 border border-black" selected={date} onChange={(date) => setDate(date as Date)} />
+            {selectedTour.title === i18n.t("tours.turtlesAndWhales.title") ?
+              <DatePicker minDate={minDate} maxDate={maxDate} className="w-full rounded-full pl-9 pr-4 py-2 border border-black" selected={date} onChange={(date) => setDate(date as Date)} />
+              :
+              <DatePicker className="w-full rounded-full pl-9 pr-4 py-2 border border-black" selected={date} onChange={(date) => setDate(date as Date)} />
+            }
           </article>
         </div>
         <div className="flex items-end space-x-3">
